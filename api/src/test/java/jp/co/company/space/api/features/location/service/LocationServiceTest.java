f7152ff49e674a2b92ccd3587169fe4a02249a78
Nicolas Laerolf <nicolas.floreal@hotmail.com>
2025-02-05 07:59:14 +0900

    :white_check_mark: test if the initial records for entities are loaded to the database after a service has been initialized

--- api/src/test/java/jp/co/company/space/api/features/location/service/LocationServiceTest.java
+++ api/src/test/java/jp/co/company/space/api/features/location/service/LocationServiceTest.java
@@ -1,5 +1,7 @@
 package jp.co.company.space.api.features.location.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 import io.helidon.microprofile.testing.junit5.HelidonTest;
@@ -21,5 +23,8 @@ public class LocationServiceTest {
     void whenCreated_dataIsLoaded() {
         // When
         service.getAll();
+
+        // Then
+        assertFalse(service.getAll().isEmpty());
     }
 }
--- api/src/test/java/jp/co/company/space/api/features/spaceStation/service/SpaceStationServiceTest.java
+++ api/src/test/java/jp/co/company/space/api/features/spaceStation/service/SpaceStationServiceTest.java
@@ -1,5 +1,7 @@
 package jp.co.company.space.api.features.spaceStation.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 import io.helidon.microprofile.testing.junit5.HelidonTest;
@@ -20,5 +22,8 @@ public class SpaceStationServiceTest {
     void whenCreated_dataIsLoaded() {
         // When
         service.getAll();
+
+        // Then
+        assertFalse(service.getAll().isEmpty());
     }
 }
