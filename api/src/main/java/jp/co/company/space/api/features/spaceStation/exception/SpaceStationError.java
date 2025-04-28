57941261b1561a13143327b92c2ec9174012ba38
Nicolas Laerolf <nicolas.floreal@hotmail.com>
2025-04-29 08:54:51 +0900

    :loud_sound: Adjust SpaceStationError messages

--- api/src/main/java/jp/co/company/space/api/features/spaceStation/exception/SpaceStationError.java
+++ api/src/main/java/jp/co/company/space/api/features/spaceStation/exception/SpaceStationError.java
@@ -6,17 +6,17 @@ import jp.co.company.space.api.shared.exception.DomainError;
  * An enum with space station error messages.
  */
 public enum SpaceStationError implements DomainError {
-    MISSING("spaceStation.missing", "A space space station is missing."),
-    MISSING_ID("spaceStation.missingId", "The ID of a space space station is missing."),
-    MISSING_NAME("spaceStation.missingName", "The name of a space space station is missing."),
-    MISSING_CODE("spaceStation.missingCode", "The code of a space space station is missing."),
-    MISSING_LOCATION("spaceStation.missingLocation", "The location of a space space station is missing."),
+    MISSING("spaceStation.missing", "A space station is missing."),
+    MISSING_ID("spaceStation.missingId", "The ID of a space station is missing."),
+    MISSING_NAME("spaceStation.missingName", "The name of a space station is missing."),
+    MISSING_CODE("spaceStation.missingCode", "The code of a space station is missing."),
+    MISSING_LOCATION("spaceStation.missingLocation", "The location of a space station is missing."),
 
-    FIND_BY_ID("spaceStation.findById", "Failed to find a space space station with the provided ID."),
-    GET_ALL("spaceStation.getAll", "Failed to get all existing space space stations."),
-    SAVE("spaceStation.save", "Failed to save a space space station."),
-    MERGE("spaceStation.merge", "Failed to merge a space space station."),
-    SAVE_LIST("spaceStation.saveList", "Failed to save space stations."),
+    FIND_BY_ID("spaceStation.findById", "Failed to find a space station with the provided ID."),
+    GET_ALL("spaceStation.getAll", "Failed to get all existing space stations."),
+    SAVE("spaceStation.save", "Failed to save a space station."),
+    MERGE("spaceStation.merge", "Failed to merge a space station."),
+    SAVE_LIST("spaceStation.saveList", "Failed to save stations."),
 
     START_SERVICE("spaceStation.serviceStartUp", "Failed to start up the space station service."),
     LOAD_INITIAL_DATA("spaceStation.loadInitialData", "Failed to load the initial space stations into the database.");
