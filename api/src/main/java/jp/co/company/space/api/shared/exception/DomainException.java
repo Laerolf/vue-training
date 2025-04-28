ba98e196ace5f6e1d70d554de14598665069890d
Nicolas Laerolf <nicolas.floreal@hotmail.com>
2025-04-29 08:58:02 +0900

    :recycle: Make DomainException abstract

--- api/src/main/java/jp/co/company/space/api/shared/exception/DomainException.java
+++ api/src/main/java/jp/co/company/space/api/shared/exception/DomainException.java
@@ -1,39 +1,24 @@
 package jp.co.company.space.api.shared.exception;
 
-import jp.co.company.space.api.features.booking.exception.BookingError;
-
 /**
  * An abstract class representing a domain specific {@link IllegalArgumentException} instance.
  */
-// TODO: make this abstract
-public class DomainException extends IllegalArgumentException {
+public abstract class DomainException extends IllegalArgumentException {
 
-    private final DomainError error;
+    private final String key;
 
     public DomainException(DomainError error) {
         super(error.getDescription());
-        this.error = error;
-    }
-
-    // TODO: Remove
-    public DomainException(String error) {
-        super(error);
-        this.error = BookingError.FIND_BY_ID;
-    }
-
-    // TODO: Remove
-    public DomainException(String error, Throwable throwable) {
-        super(error, throwable);
-        this.error = BookingError.FIND_BY_ID;
+        this.key = error.getKey();
     }
 
     public DomainException(DomainError error, Throwable throwable) {
         super(error.getDescription(), throwable);
-        this.error = error;
+        this.key = error.getKey();
     }
 
     public String getKey() {
-        return error.getKey();
+        return key;
     }
 
     @Override
