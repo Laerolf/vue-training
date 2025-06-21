94d1cbd3afc4ea308adf0709f6835ea5939c5558
Nicolas Laerolf <nicolas.floreal@hotmail.com>
2025-06-22 08:11:20 +0900

    ➕ Add esbuild + :fire: remove vitest, prettier and eslint

---
+++ test-runner/esbuild.mjs
@@ -0,0 +1,21 @@
+import { build } from 'esbuild'
+import {rmSync,cpSync, copyFileSync} from 'node:fs'
+
+rmSync("dist", {recursive: true, force: true})
+cpSync('lib', 'dist/lib', { recursive: true })
+copyFileSync('reporter.ts', 'dist/reporter.ts')
+
+build({
+    entryPoints: ['src/main.ts', 'playwright.config.ts'],
+    outdir: "dist",
+    bundle: true,
+    platform: 'node',
+    target: 'node22',
+    format: 'cjs',
+    sourcemap: true,
+    packages: "bundle",
+    external: [
+        "chromium-bidi",
+        "playwright"
+    ]
+}).catch(() => process.exit(1))
